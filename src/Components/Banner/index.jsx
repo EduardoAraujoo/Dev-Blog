0e5a35b7cd105bc750cd68664df84890a8917ed9
Eduardo de Oliveira Araujo <duduoliveira1911@gmail.com>
2023-06-15 16:32:47 -0300

    trying to inplement Links

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -1,22 +1,17 @@
-import LinkMain from '../LinkMain'
 import styles from './Banner.module.scss'
 
 export default function Banner() {
   return (
     <header className={styles.banner}>
       <section className={styles.section}><h1>Dev Blog</h1></section>
-      
+
       <article className={styles.article}>
-        <ul className={styles.article__ul}>
-          <li className={styles.article__li}>Home</li>
-          <li className={styles.article__li}>
-            <LinkMain className={styles.article__li} to="/aboutme">
-            About me
-            </LinkMain>
-            </li>
-          <li className={styles.article__li}>Posts</li>
-          <li className={styles.article__li}>Contact Me</li>
-        </ul>
+        <nav className={styles.article__nav}>
+          <p>Home</p>
+          <p>About me</p>
+          <p>Posts</p>
+          <p>Contact Me</p>
+        </nav>
       </article >
 
       <section className={styles.section}><div className={styles.section__circle}></div></section>
@@ -25,5 +20,5 @@ export default function Banner() {
   )
 }
 
- 
+
 
